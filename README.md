3e5aa00693f52ba5b2304e57ec71675ed0d31a5c
schlopp96 <71921821+schlopp96@users.noreply.github.com>
2021-12-10 23:52:19 -0600

    # GitHub PFP Scraper v1.0.0
    > Initial Upload
    
    ---
    
    - Initialized Repository.
    - Created README.md.
    - Created .gitignore
    
    - Uploaded as a personal project based upon the following tutorial by <http://freecodecamp.org/>:
      - <https://youtu.be/SqvVm3QiQVk?t=37>
        - Listed as first project in video description, titled "Web Scraping Project".
    
    Signed-off-by: schlopp96 <71921821+schlopp96@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,26 @@
+# GitHub PFP Scraper Project
+
+> v1.0.0 Initial Release
+
+---
+
+- Initialized Repository.
+- Created README.md.
+- Created .gitignore
+
+- Uploaded as a personal project thanks to following tutorial by <http://freecodecamp.org/>:
+  - <https://youtu.be/SqvVm3QiQVk?t=37>
+    - Listed as first project in video description, titled "Web Scraping Project".
+
+## How Does it Work?
+
+---
+
+1. Simply download/extract folder anywhere you'd like to install.
+2. Run `"./src/GHub_PFP_Scraper.py"` to start the program.
+3. Enter a Github username to begin searching for matching profile
+4. If a valid profile is found, it will be opened using your default web browser.
+   - Otherwise, you will be prompted to try again.
+5. To exit, you may enter "exit" at any time while prompted for input.
+
+---
---
+++ src/GHub_PFP_Scraper.py
@@ -0,0 +1,74 @@
+#/usr/bin/env python3
+
+#> Small script that opens github profile pictures associated with the entered username.
+#@ Based off of http://freecodecamp.org/ tutorial video: https://youtu.be/SqvVm3QiQVk?t=37
+
+#import PySimpleGUI as sg
+from time import sleep as s
+from typing import Any, NoReturn
+from webbrowser import open as open_window
+
+from bs4 import BeautifulSoup as bs
+from requests import get
+
+
+def profile_search(username: str) -> bool | None:
+    """
+    Small script that opens the profile-picture of the entered Github username within your default browser.
+
+    - If there is already a browser window opened, the image will be loaded as a new tab within the window.
+
+    :param username: username of the GitHub profile to search for.
+    :type username: str
+    :return: Opens found profile picture in user's default browser as a new window, or as a new tab if there is already a window open.
+    :rtype: Any
+    """
+    if username is not None:
+        try:
+            url = f'https://github.com/{username}'
+            gh_profile = get(url)
+            #:NOTE - #* "content" represents all data contained within an html response.
+            soup = bs(gh_profile.content, 'html.parser')
+            #:NOTE - #? Finds info by inspecting profile picture element in my browser.
+            profile_img: str = soup.find('img', {'alt': 'Avatar'})['src']
+
+            #:NOTE - #!Open found prof-pic in browser:
+            print("\nOk!\n")
+            return open_window(profile_img)
+
+    #& Raise exception if username can't be found, or is invalid.
+        except TypeError:
+            print(
+                f'\n\t~   ERROR   ~\nUnable to find user "{username}" profile.\n\nPlease try again.\n'
+            )
+            s(1.2)
+
+
+def main() -> NoReturn:
+    """
+    Program function wrapper.
+
+    - NOT MEANT TO BE CALLED MANUALLY!schlopp
+    - Enter "exit" to close program when prompted for username.
+    - Prints "how-to-exit" reminders every 3rd entry.
+
+    :return: Primary flow of program.
+    :rtype: NoReturn
+    """
+    searchCount: int = 0
+
+    while True:
+        if searchCount % 3 == 0:
+            print(
+                '\nNote:\nYou may enter "Exit" at any point to close application.\n'
+            )
+        profile: str = input('Enter a GitHub username:\n-> ')
+        if profile.lower() == 'exit':
+            break
+        profile_search(profile)
+        searchCount += 1
+    exit()
+
+
+if __name__ == '__main__':
+    main()
